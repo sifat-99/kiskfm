9ac45ab44ae6ff7b938081bbb83f703f4d95899a
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2024-08-19 03:48:03 +0600

    Update padding for gallery

--- src/app/(gallery)/gallery/page.js
+++ src/app/(gallery)/gallery/page.js
@@ -4,7 +4,7 @@ import React from "react";
 const Gallery = () => {
   return (
     <>
-      <div className="min-h-[50vh]">
+      <div className="min-h-[50vh] px-4">
         <GalleryPhoto />
       </div>
     </>
